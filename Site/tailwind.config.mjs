e3457a9823ac0952f24523401046a6ec769d3a39
Lilyaddy15 <92546096+Lilyaddy15@users.noreply.github.com>
2025-04-10 16:52:23 -0400

    Add email signup form and thank you page

--- Site/tailwind.config.mjs
+++ Site/tailwind.config.mjs
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
-        'deep-navy': '#15273F',
-        'soft-blue': '#DFE9F6',
-        'medium-blue': '#ADC9E9',
-        'bright-blue': '#6093D4',
-        'neutral-gray': '#AEAEAE'
+        "deep-navy": "#15273F",
+        "navy-light": "#1E3859",
+        "soft-blue": "#DFE9F6",
+        "medium-blue": "#ADC9E9",
+        "bright-blue": "#6093D4",
+        "neutral-gray": "#AEAEAE",
       },
       fontFamily: {
-        'avenir': ['Avenir', 'sans-serif']
-      }
-    }
+        avenir: ["Avenir", "sans-serif"],
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
