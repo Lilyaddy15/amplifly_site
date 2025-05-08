7f8f3cb7eee48019656d2d3d5b5b68ea2c75bec9
Lilyaddy15 <92546096+Lilyaddy15@users.noreply.github.com>
2025-05-08 15:01:01 -0400

    Add About page and update navigation with working links

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "deep-navy": "#15273F",
+        "navy-light": "#1E3859",
+        "soft-blue": "#DFE9F6",
+        "medium-blue": "#ADC9E9",
+        "bright-blue": "#6093D4",
+        "neutral-gray": "#AEAEAE",
+        "accessible-gray": "#666666",
+      },
+      fontFamily: {
+        avenir: ["Avenir", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
