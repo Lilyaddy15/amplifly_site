d1e0426e5442ad3e5a8c7ae0dda0dc4032e8ab77
Lilyaddy15 <92546096+Lilyaddy15@users.noreply.github.com>
2025-05-12 18:02:31 -0400

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: "static",
+  integrations: [tailwind()],
+  adapter: netlify(),
 });
